1f0837625cad55ca1f255a20b69eb22679d783ce
Anatolij Ostroumov <ostroumov095@gmail.com>
2017-04-28 14:22:45 +0300

    fix saving metadata to journalctl

--- journalctl.go
+++ journalctl.go
@@ -5,6 +5,7 @@ package vdlog
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 // Mapping between journalctl and vdlog levels
@@ -74,7 +75,7 @@ func createJournaldSink(host string, port int, tcp, local bool) func(e Event) er
 		fmt.Fprintf(stdin, "VDLOG_LEVEL=%s\n", e.LevelString)
 		fmt.Fprintf(stdin, "VDLOG_CALLED=%s\n", e.Called)
 		for k,v := range e.Metadata {
-			fmt.Fprintf(stdin, "VDLOG_META_%s=%q\n", k, fmt.Sprint(v))
+			fmt.Fprintf(stdin, "VDLOG_META_%s=%q\n", strings.ToUpper(k), fmt.Sprint(v))
 		}
 		err = stdin.Close()
 		if err != nil {
